59f77a0ab2e178660d4c7e19b7e93d1343ffc45e
Anurag Singh <anuragrajput9871595773@gmail.com>
2025-05-14 09:17:40 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,7 +32,7 @@ const obj4 = {5: "a", 6: "b"}
 //  const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2}   // Spread operator
-console.log(obj3);
+//console.log(obj3);
 
 
 const users = [
@@ -51,12 +51,39 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+//console.log(tinderUser.hasOwnProperty('isLogged'));
 
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "anurag"
+}
+
+//console.log(course.courseInstructor);
+
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name": "anurag",
+//     "coursename": "js in hindi",  //json
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
