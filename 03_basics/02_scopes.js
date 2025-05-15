45901d2f73c55e314588611b7426a0aa835f8e62
Anurag Singh <anuragrajput9871595773@gmail.com>
2025-05-15 10:25:15 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,7 +5,7 @@ let a = 400
 if (true) {
     let a = 10
     const b =20
-    console.log("INNER: ", a);
+    //console.log("INNER: ", a);
     
 
 }
@@ -13,8 +13,56 @@ if (true) {
 
 
 
-console.log(a);
+//console.log(a);
 // console.log(b);
 //console.log(c);
 
+function One(){
+      const username = "anurag"
+        
+      function two() {
+        const website = "youtube"
+        console.log(username);
+               
+      }
+      //console.log(website);
+      
+      two()
+      
+    }
 
+   // One()
+
+   if (true) {
+    const username ="anurag"
+    if (username === "anurag") {
+        const website = "youtube"
+        //console.log(username + website);
+        
+    }
+   // console.log(website);
+    
+   }
+
+ //  console.log(username);
+   
+
+ // ++++++++++++++++++++++++++++++++ intersting  ++++++++++++++++++++
+ 
+console.log(addone(5));
+
+
+ function addone(num){
+          return num + 1
+ }
+
+
+ 
+
+addtwo(4)
+ const addtwo  = function(num){
+    return num + 2
+ }
+
+
+// addtwo(4)
