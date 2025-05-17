c14c5391c85d1c9686cfa5028498c7d218d2fdfd
Anurag Singh <anuragrajput9871595773@gmail.com>
2025-05-17 10:12:34 +0000

    if else statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,67 @@
+ // if
+
+const isUserloggedIn = true
+const temperature = 40
+
+
+
+
+//  if (temperature === 56) {
+//     console.log("less than 50");
+    
+//  } else{
+//        console.log("temperature is greater than 50");
+//  }
+// console.log("executed");
+
+
+ // < , > , <= , >=, ==, !=, === , !== 
+
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+
+//const balance = 1000
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");
+
+
+// if (balance < 400) {
+//     console.log("less than");
+    
+// }else if (balance < 599) {
+//      console.log("less than 599");
+     
+// } else if (balance < 800) {
+//     console.log("less than 800");
+    
+// } else {
+//     console.log("less than 1100");
+    
+// }
+
+
+
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+if (UserloggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail ) {
+    console.log("User logged in");
+    
+}
