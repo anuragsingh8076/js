a8455c0559684993703e104e2c641eb2d4fd13eb
Anurag Singh <anuragrajput9871595773@gmail.com>
2025-05-27 10:47:37 +0000

    call and this in javascript

---
+++ 10_classesand_oop/call.js
@@ -0,0 +1,18 @@
+function SetUsername(username){
+    //Complex db calls
+    this.username = username
+    console.log("Called");
+}
+
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+
+
+    this.email = email
+    this.password = password
+}
+
+
+const chai = new createUser("chai", "chai@google.com", "123")
+console.log(chai);
