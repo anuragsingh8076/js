f0b666f7ed83c5a27ce38721c13a24c56a434307
Anurag Singh <anuragrajput9871595773@gmail.com>
2025-05-28 08:08:36 +0000

    Static properties in javascript

---
+++ 10_classesand_oop/staticprop.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(username){
+        this.username = username
+
+    }
+
+    logMe(){
+    console.log(`Username: ${this.username}`);
+    
+    }
+
+   static createId(){
+        return `123`
+    }
+}
+
+
+const anurag = new User("anurag")
+//console.log(anurag.createId())
+
+
+class Teacher extends User {
+    constructor(username,email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.logMe());
